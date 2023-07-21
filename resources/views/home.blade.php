8b9918487bf2318bf48973316b0a14dafc5d9fe2
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-07-21 13:05:57 +0200

    home2 update
    Build the footer section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -188,6 +188,132 @@
         </nav>
       </div>
       {{--|------------------------------- END HIDDEN NAVIGATION |----------------------------------}}
+      {{-- |------------------------------ FOOTER |-------------------------------------------------------}}
+      <footer class="w-full bg-gray-100">
+        <div class="w-[90%] mx-auto">
+          {{-- Navigation --}}
+          <nav class="hidden">
+            <ul>
+              <a href="#">
+                <li>Home</li>
+              </a>
+              <a href="#">
+                <li>Watches</li>
+              </a>
+              <a href="#">
+                <li>Pre-owned</li>
+              </a>
+              <a href="$">
+                <li>Jewelry</li>
+              </a>
+              <a href="#">
+                <li>Blog</li>
+              </a>
+              <a href="#">
+                <li>About topper</li>
+              </a>
+              <a href="#">
+                <li>Contact us</li>
+              </a>
+            </ul>
+          </nav>
+          {{-- company information --}}
+          <div
+            class="flex flex-col justify-center items-center border-b-[1px] md:flex-row md:flex-wrap md:py-5 lg:flex-nowrap">
+            <div class="w-full h-24 flex flex-col justify-center item-center md:w-1/2 lg:min-w-fit lg:px-2">
+              <h5 class="font-poppins text-lg font-semibold text-center">Contact Information</h4>
+                <h5 class="font-poppins text-md font-light text-center"><span class="font-semibold">888-730-2221</span>
+                  / 650-347-2221</h5>
+                <p class="font-poppins text-base font-light text-center">info@topperjewelers.com</p>
+            </div>
+            <div class="w-full h-24 flex flex-col justify-center item-center md:w-1/2 lg:min-w-fit lg:px-2">
+              <h5 class="font-poppins text-md font-semibold text-center">Operating Hours</h5>
+              <p class="font-poppins text-base font-light text-center">Tuesday-Saturday 11AM-5PM PST</p>
+            </div>
+            <div class="w-full h-24 flex flex-col justify-center item-center md:w-1/2 lg:min-w-fit lg:px-2 ">
+              <h4 class="font-poppins text-md font-semibold text-center">Connect with us</h4>
+              <div class="flex justify-center items-center">
+                <span class="block mx-2"><i class="fa-brands fa-twitter text-lg"></i></span>
+                <span class="block mx-2"><i class="fa-brands fa-facebook text-lg"></i></span>
+                <span class="block mx-2"><i class="fa-brands fa-instagram text-lg"></i></span>
+              </div>
+            </div>
+            <div class="w-full h-24 flex flex-col justify-center item-center md:w-1/2 lg:min-w-fit lg:px-2 ">
+              <h4 class="font-poppins text-md font-semibold text-center">Sign-up to our Newsletter</h4>
+              <form action="" class="flex justify-center items-center mt-2">
+                <input type="text" placeholder="Email Address" class="w-32 border bg-transparent p-1 h-9 text-xs">
+                <button
+                  class="bg-black text-white px-2 h-9 text-sm font-poppins cursor-pointer uppercase font-bold">Sign
+                  up</button>
+              </form>
+            </div>
+          </div>
+          {{-- TOPPER LOGO AND DESCRIPTION --}}
+          <div class="w-full">
+            <a class="flex justify-center items-center py-5" href="#">
+              <img class="w-[200px]" src="{{ url('/images/topper-1940-logo.svg.svg') }}" alt="">
+            </a>
+            <p class="font-poppins text-center text-base mb-5">1294 Burlingame Ave., Burlingame, CA 94010</p>
+            <p class="font-poppins text-center text-base mb-5">In the heart of Silicon Valley, Topper Fine Jewelers is a
+              family-owned store in the charming town of
+              Burlingame,
+              California. We are located half way in-between San Francisco and San Jose. We are an authorized dealer of
+              a unique brand
+              mix including: OMEGA, Grand Seiko, Blancpain, Glashütte Original, Nomos Glashütte, Longines, Zenith,
+              Bremont, Zodiac,
+              Seiko Presage, Seiko Prospex, Oris, G-Shock, Ernst Benz, Rado, Hamilton, Michele, Mühle Glashütte, and
+              more. We also
+              feature an extensive collection of bridal and fashion jewelry highlighted by brands such as Tacori, Marco
+              Bicego, and
+              Roberto Coin.
+            </p>
+            <span class="flex justify-center items-center mb-5">
+              <a href=""><img src="{{ url('/images/bbb-badge.avif') }}" alt=""></a>
+            </span>
+            <p class="text-center font-poppins text-xs mb-5">Reproduction in whole or in part in any form or medium
+              without
+              express written
+              permission of Topper Fine
+              Jewelers is
+              prohibited.
+            </p>
+            <p class="text-center font-poppins text-xs mb-5">All descriptions of weight and size are approximate. While
+              we do our best to maintain our website's
+              accuracy and try and
+              fix typographical errors when we are made aware of them, we reserve the right to unilaterally correct any
+              mistake in
+              image, price,or description that appears on our site.
+            </p>
+            {{-- Payment Logo --}}
+            <div class="w-fit mx-auto flex justify-between items-center mb-5">
+              <span class="block mx-1"><i class="fa-brands fa-cc-amex text-2xl md:text-4xl"></i></span>
+              <span class="block mx-1"><i class="fa-brands fa-cc-apple-pay text-2xl md:text-4xl"></i></span>
+              <span class="block mx-1"><i class="fa-brands fa-cc-diners-club text-2xl md:text-4xl"></i></span>
+              <span class="block mx-1"><i class="fa-brands fa-cc-discover text-2xl md:text-4xl"></i></span>
+              <span class="block mx-1"><i class="fa-brands fa-cc-jcb text-2xl md:text-4xl"></i></span>
+              <span class="block mx-1"><i class="fa-brands fa-cc-apple-pay text-2xl md:text-4xl"></i></span>
+              <span class="block mx-1"><i class="fa-brands fa-cc-mastercar text-2xl md:text-4xl"></i></span>
+              <span class="block mx-1"><i class="fa-brands fa-cc-visa text-2xl md:text-4xl"></i></span>
+              <span class="block mx-1"><i class="fa-brands fa-cc-paypal text-2xl md:text-4xl"></i></span>
+            </div>
+            {{-- PRIVACY POLICY, RETURN POLICY, TERMS AND CONDITIONS --}}
+            <div
+              class="w-full flex justify-center items-center flex-wrap mb-5 border-t-[1px] border-b-[1px] py-3 md:flex-nowrap md:w-[600px] md:mx-auto">
+              <span class="block mb-5 font-poppins uppercase text-center text-base w-1/2"><a href="#">Privacy
+                  Policy</a></span>
+              <span class="block mb-5 font-poppins uppercase text-center text-base w-1/2"><a href="#">Return
+                  Policy</a></span>
+              <span class="block mb-5 font-poppins uppercase text-center text-base w-1/2"><a href="#">Terms &
+                  Condition</a></span>
+            </div>
+            {{-- BOTTOM FOOTER --}}
+            <div class="text-sm text-center font-poppins">
+              2023 Topper Fine Jewelers - Powered by Laravel
+            </div>
+          </div>
+        </div>
+      </footer>
+      {{-- |------------------------------ END FOOTER |-------------------------------------------------------}}
     </main>
     <script src="{{ url('/js/home.js') }}"></script>
   </body>
