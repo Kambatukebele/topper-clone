b5b46e1c8dc65827fdd5a0331d11d4bd3642d684
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-07-26 09:55:26 +0200

    update home9

--- resources/views/components/navigation/navigation.blade.php
+++ resources/views/components/navigation/navigation.blade.php
@@ -29,3 +29,117 @@
         </a>
     </ul>
 </nav>
+
+{{-- Watches Mega menu --}}
+<section>
+    <div>
+        {{-- Top Brands --}}
+        <div>
+            <h4>Top brands</h4>
+            <div>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Shop All Brand</li>
+                </a>
+            </div>
+        </div>
+        {{-- second column --}}
+        <div>
+            {{-- Availability --}}
+            <h4>Availability</h4>
+            <div>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+            </div>
+            {{-- Price range --}}
+            <h4>Price range</h4>
+            <div>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Shop All Brand</li>
+                </a>
+            </div>
+            <h4>Accessories</h4>
+            <div>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Lorem</li>
+                </a>
+            </div>
+        </div>
+        {{-- Picture column --}}
+        <div>
+            <a href="#">
+                <div>
+                    <img src="{{ url('/images/19bf481c3d90749d0d4dcb567bb9e26ffa7b0bd427e3a9f5c012d5956152a1ea.jpeg') }}"
+                        alt="">
+                </div>
+                <div>
+                    Omega Watches
+                </div>
+            </a>
+            <a href="#">
+                <div>
+                    <img src="{{ url('/images/1776af86c6c89504f566907daf96d2da0328627a0de9fdde33a227dfdec6f30e.jpeg') }}"
+                        alt="">
+                </div>
+                <div>
+                    Omega Watches
+                </div>
+            </a>
+            <a href="#">
+                <div>
+                    <img src="{{ url('/images/497179c4d13bd89dd05bd7f23f42a7e882ad1b2111c10659fd73354ab4aa97da.jpeg') }}"
+                        alt="">
+                </div>
+                <div>
+                    Omega Watches
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
+{{-- End Watches Mega menu --}}
