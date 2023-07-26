5f1d5e6aa0a8c64441839a3e612859a07efc191d
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-07-26 22:11:49 +0200

    update

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,4 @@
+//Trigger hamburger Menu on small devices
 const TriggerMenu = () => {
     let hiddenMenu = document.getElementsByClassName("hiddenMenu")[0];
     let closeBtn = document.getElementsByClassName("closeBtn")[0];
@@ -17,3 +18,21 @@ const TriggerMenu = () => {
 };
 
 TriggerMenu();
+
+//On Hover, show Megamenu
+const showHideMenu = () => {
+    let megaMenu = document.getElementsByClassName("megaMenu")[0];
+    let watches = document.getElementsByClassName("watches")[0];
+    let megaMenuSub = document.getElementsByClassName("megaMenuSub")[0];
+
+    watches.addEventListener("mouseenter", (e) => {
+        e.preventDefault();
+        megaMenu.classList.add("xl:block");
+    });
+
+    megaMenuSub.addEventListener("mouseleave", (e) => {
+        e.preventDefault();
+        megaMenu.classList.remove("xl:block");
+    });
+};
+showHideMenu();
--- resources/views/components/navigation/navigation.blade.php
+++ resources/views/components/navigation/navigation.blade.php
@@ -1,10 +1,10 @@
-<nav class="hidden lg:block lg:w-full">
+<nav class="hidden lg:block lg:w-full lg:relative">
     <ul class="lg:w-[90%] lg:mx-auto lg:flex lg:justify-between lg:items-center">
         <a href="{{ route('home') }}" class="lg:block lg:border lg:uppercase lg:text-center lg:w-[15%] lg:py-2">
             <li>Home</li>
         </a>
         <a href="{{ route('collections.watches') }}"
-            class="lg:block lg:border lg:uppercase lg:text-center lg:w-[15%] lg:py-2">
+            class="lg:block lg:border lg:uppercase lg:text-center lg:w-[15%] lg:py-2 watches">
             <li>Watches</li>
         </a>
         <a href="{{ route('collections.pre-owned') }}"
@@ -31,111 +31,118 @@
 </nav>
 
 {{-- Watches Mega menu --}}
-<section class="w-full h-[500px] bg-white">
-    <div class="w-[90%] mx-auto h-full">
+<section class="hidden xl:w-full xl:h-[450px] megaMenu xl:absolute  xl:top-48 xl:left-0 xl:right-0 z-10 ">
+    <div
+        class="hidden xl:flex xl:justify-between xl:items-center xl:w-[90%] xl:mx-auto xl:border xl:bg-white xl:p-5 xl:shadow-lg megaMenuSub">
         {{-- Top Brands --}}
-        <div>
-            <h4>Top brands</h4>
-            <div>
-                <a href="#">
-                    <li>Lorem</li>
+        <div class="hidden xl:block xl:w-[145px] h-[420px]">
+            <h4 class="font-poppins text-md uppercase font-bold text-black">Top brands</h4>
+            <ul class="hidden xl:block xl:list-none xl:w-full">
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Omega</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Grand Seiko</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Breitling</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">H. Moser & Cie</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Czapek</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Zenith</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Glashütte Original</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Parmigiani Fleurier</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Blancpain</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Oris</li>
                 </a>
-                <a href="#">
-                    <li>Shop All Brand</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Seiko Luxe</li>
                 </a>
-            </div>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Seiko 5</li>
+                </a>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Shop All Brand</li>
+                </a>
+            </ul>
         </div>
         {{-- second column --}}
-        <div>
+        <div class="hidden xl:block xl:w-[145px] h-[420px]">
             {{-- Availability --}}
-            <h4>Availability</h4>
-            <div>
-                <a href="#">
-                    <li>Lorem</li>
+            <h4 class="font-poppins text-md uppercase font-bold text-black">Availability</h4>
+            <ul class="hidden xl:block xl:list-none xl:w-full">
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">In-Stock</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Pre-Order</li>
                 </a>
-            </div>
+            </ul>
             {{-- Price range --}}
-            <h4>Price range</h4>
-            <div>
-                <a href="#">
-                    <li>Lorem</li>
+            <h4 class="font-poppins text-md uppercase font-bold text-black">Price range</h4>
+            <ul class="hidden xl:block xl:list-none xl:w-full">
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Below $1000</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">$1000 to $3000</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">$3000 to $5000</li>
                 </a>
-                <a href="#">
-                    <li>Shop All Brand</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Above $5000</li>
                 </a>
-            </div>
-            <h4>Accessories</h4>
-            <div>
-                <a href="#">
-                    <li>Lorem</li>
+            </ul>
+            <h4 class="font-poppins text-md uppercase font-bold text-black">Accessories</h4>
+            <ul class="hidden xl:block xl:list-none xl:w-full">
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Watch Winders</li>
                 </a>
-                <a href="#">
-                    <li>Lorem</li>
+                <a href="#" class="hidden xl:block my-1">
+                    <li class="text-base font-normal font-poppins text-black">Watch Boxes & Cases</li>
                 </a>
-            </div>
+            </ul>
         </div>
         {{-- Picture column --}}
-        <div>
-            <a href="#">
-                <div>
+        <div class="hidden xl:flex xl:justify-between xl:items-center xl:w-[850px]">
+            <a href="#" class="hidden xl:block xl:w-[260px] xl:h-[420px]">
+                <div class="xl:w-[260px] xl:h-[325px]">
                     <img src="{{ url('/images/19bf481c3d90749d0d4dcb567bb9e26ffa7b0bd427e3a9f5c012d5956152a1ea.jpeg') }}"
-                        alt="">
+                        alt="" class="xl:w-full xl:h-full xl:object-cover xl:object-center">
                 </div>
-                <div>
+                <div class="text-center font-bold font-poppins text-base w-full mt-2">
                     Omega Watches
                 </div>
             </a>
-            <a href="#">
-                <div>
+            <a href="#" class="hidden xl:block xl:w-[260px] xl:h-[420px]">
+                <div class="xl:w-[260px] xl:h-[325px]">
                     <img src="{{ url('/images/1776af86c6c89504f566907daf96d2da0328627a0de9fdde33a227dfdec6f30e.jpeg') }}"
-                        alt="">
+                        alt="" class="xl:w-full xl:h-full xl:object-cover xl:object-center">
                 </div>
-                <div>
+                <div class="text-center font-bold font-poppins text-base w-full mt-2">
                     Omega Watches
                 </div>
             </a>
-            <a href="#">
-                <div>
+            <a href="#" class="hidden xl:block xl:w-[260px] xl:h-[420px]">
+                <div class="xl:w-[260px] xl:h-[325px]">
                     <img src="{{ url('/images/497179c4d13bd89dd05bd7f23f42a7e882ad1b2111c10659fd73354ab4aa97da.jpeg') }}"
-                        alt="">
+                        alt="" class="xl:w-full xl:h-full xl:object-cover xl:object-center">
                 </div>
-                <div>
+                <div class="text-center font-bold font-poppins text-base w-full mt-2">
                     Omega Watches
                 </div>
             </a>
