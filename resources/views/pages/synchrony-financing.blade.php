ca095a33ca6ac592df31a8715591d389c84ae4b1
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-08-08 22:09:06 +0200

    update

--- resources/views/pages/synchrony-financing.blade.php
+++ resources/views/pages/synchrony-financing.blade.php
@@ -28,7 +28,75 @@
       {{--|END HIDDEN NAVIGATION |-}}
 
       {{--| CONTENT |--}}
-      <h1>Synchro</h1>
+      <section class="w-[90%] mx-auto flex justify-center items-center flex-col my-10 lg:w-[900px]">
+        <img src="{{ url('/images/download (9).webp') }}" class="mb-5" alt="">
+        <p class="font-poppins text-center text-gray-600 font-normal my-5">NOTE: As of August 19, 2014, GE Capital
+          Retail
+          Finance (GECRF), the provider of Business Center, will now be
+          known as
+          Synchrony Financial. Please note that some of the program documents contained below still refer to GE Capital,
+          GECRF
+          and/or GE Capital Retail Bank (GECRB), but will be changing to reference Synchrony Financial and/or Synchrony
+          Bank in
+          the near future.</p>
+        <h2 class="font-poppins text-center text-3xl mx-9 mb-5">No Interest if Paid in Full within 6 or 12 months</h2>
+        <p class="font-poppins text-center text-gray-600 font-normal my-2">To apply for financing with Payments/Deferred
+          Interest** with our partner G.E. Capital, Simply download 1)
+          The main
+          application 2) The 1Payments/Deferred Interest for promotional period document and fill out the application.
+          <span class="font-semibold">After
+            reviewing both documents, please fax them to us with a cover letter to 650-344-9402</span>. PLEASE DO NOT
+          E-MAIL YOUR
+          APPLICATION, as E-mailing personal information is not secure. Credit is subject to approval by G.E. Capital,
+          and will be
+          established by G.E. Financial. Certain exclusions may apply. Please call us before faxing an application so
+          that we may
+          review the price of any item and suitability of this promotion for an item that meets your interest.
+        </p>
+        <p class="font-poppins text-center text-gray-600 font-normal my-3">*Offer Valid on Most Jewelry and Watches on
+          purchases of $500 dollars or more with your Topper Fine Jewelers
+          G.E.
+          Capital Card. Interest will be charged to your account from the purchase date if the promotional purchase is
+          not paid in
+          full within 6 or 12 months. Minimum Monthly Payments Required.</p>
+        <p class="font-poppins text-center text-gray-600 font-normal my-3">*No interest will be charged on the promo
+          purchase if you pay the promo purchase amount in full within6 or 12
+          months
+          term. If you do not, interest will be assessed on the promo purchase from the purchase date. Regular account
+          terms apply
+          to non-promotional purchases and, after promotion ends, to promotional balance. For new accounts: Purchase APR
+          is
+          29.9Minimum Interest Charge is $2. Existing cardholders should see their credit card agreement for their
+          applicable
+          terms. Subject to credit approval.</p>
+        <a class="font-poppins text-center uppercase font-semibold cursor-pointer bg-yellow-300 text-black block w-40 py-6 my-5"
+          href="{{ url('https://www.mysynchrony.com/mmc/LX77374100') }}">
+          Apply now
+        </a>
+        <div class="bg-gray-100 p-5 w-full flex justify-center items-center flex-col lg:w-[900px]">
+          <div class="md:flex md:justify-center md:items-center">
+            <div class="bg-gray-200 mx-5 mb-5 p-5 md:mb-0">
+              <h4 class="font-poppins text-2xl text-center">Application Form</h4>
+              <p class="font-poppins text-center text-gray-600 font-light my-5">Main application to establish a line of
+                credit with G.E. Money for use at Topper Fine Jewelers.</p>
+              <a class="font-poppins text-center underline text-black font-semibold my-5" href="#">Click Here to
+                download</a>
+            </div>
+            <div class="bg-gray-200 mx-5 p-5">
+              <h4 class="font-poppins text-2xl text-center">No Interest Promotion Form</h4>
+              <p class="font-poppins text-center text-gray-600 font-light my-5">Terms and conditions for the promotional
+                no interest if paid within 6 or 12 months offer.*</p>
+              <a class="font-poppins text-center text-black font-semibold underline my-5" href="#">Click Here to
+                download</a>
+            </div>
+          </div>
+          <div class="font-poppins text-center text-gray-600 font-light my-5">
+            *Terms, conditions, and certain exclusions such as loose diamonds and some brands apply. Financing requires
+            approval of
+            credit by G.E. Money Bank. Please read all documents above carefully to understand all terms and conditions.
+          </div>
+        </div>
+      </section>
       {{--| END CONTENT |--}}
 
 
--- resources/views/pages/watch-jewelry-trade-in.blade.php
+++ resources/views/pages/watch-jewelry-trade-in.blade.php
@@ -1,1 +1,161 @@
-<h1>Watch Jewelry</h1>
+<!DOCTYPE html>
+<html lang="en">
+  {{-- HEAD --}}
+  <x-header.head />
+  {{-- HEAD --}}
+
+  <body>
+    <main class="font-poppins">
+      {{--| INFORMATION SECTION |--}}
+      <x-information_section.information_section />
+      {{--| END INFORMATION SECTION |--}}
+
+      {{--| HEADER |--}}
+      <x-header.header />
+      {{--| END HEADER |--}}
+
+
+      {{--|--NAVIGATION |--}}
+      <x-navigation.navigation />
+      {{--| END NAVIGATION |--}}
+
+      {{--| HIDDEN HEADER |---}}
+      <x-header.hidden_header />
+      {{--|END HIDDEN HEADER |--}}
+
+      {{--| HIDDEN NAVIGATION |--}}
+      <x-navigation.hidden_navigation />
+      {{--|END HIDDEN NAVIGATION |-}}
+
+      {{--| CONTENT |--}}
+      <section class="w-[90%] mx-auto pb-5 border-b my-10">
+        <h2 class="font-poppins text-center font-normal text-3xl my-10">Whether Trading in your Watch or Jewelry, Turn
+          to
+          Topper Fine Jewelers</h2>
+        <p class="font-poppins text-center text-gray-600 font-normal my-3">Established in 1940, and under the same
+          ownership since 1947, Topper Fine Jewelers has served the San
+          Francisco Bay Area
+          for generations. In the current economic climate, an increasing number of our clients either wish to use their
+          existing
+          watches and jewelry as partial payment for a new item, or look to us to buy their items for fair value. We've
+          handled
+          thousands of transactions (many of them remotely) that involve buying or trading precious items. Our
+          experience, ethics,
+          and courteous nature cause clients to choose us over and over again. Topper Fine Jewelers is here to make your
+          transaction as fair and stress free as possible. Please click on an area below for more information.</p>
+      </section>
+      <section class="w-[90%] mx-auto">
+        <div class="flex justify-center items-center flex-col md:flex-row md:w-full md:justify-between md:mx-auto">
+          <div class="md:w-[45%] flex justify-center items-center flex-col">
+            <h3 class="font-poppins font-semibold text-xl text-center">We Accept Trade In Watches</h3>
+            <p class="font-poppins text-center text-gray-600 font-normal my-3">Topper Fine Jewelers accepts previously
+              owned watches as part or all of your payment towards your new
+              watch. Your old
+              watch may be worth more than you think!</p>
+            <p class="font-poppins text-center text-gray-600 font-normal my-3">How much is your watch worth? Value is
+              dependent on the condition and market value of your watch.
+              Topper
+              Fine Jewelers
+              provides one of the most generous trade-in allowances in the industry.</p>
+            <p class="font-poppins text-center text-gray-600 font-normal my-3">While every watch is not always right
+              for us, we will give you a general sense of what we can do over
+              the
+              phone. If we
+              do come to an agreement, we will expect you to send the watch back to us with a copy of the following
+              <span class="font-semibold">Details and
+                Declaration of Ownership</span>. When you call us please be prepared to discuss the following
+              regarding
+              a potential trade in.
+              Each factor may affect the trade in value of your watch.
+            </p>
+            <ul>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">What
+                is the new watch you are interested in purchasing?</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">What
+                is the condition of your watch case, strap, and bracelet?</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">What
+                is the working condition of your watch?</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">Does
+                the watch have the original box and paper work?</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">Does
+                the watch have a valid stamped warranty card from an authorized dealer?</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">Do you
+                have the original receipt from the purchase?</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">Can
+                you clearly see the original serial number on the watch?</li>
+            </ul>
+            <p class="font-poppins text-center text-gray-600 font-normal my-3">While we make every effort to be as
+              accurate as we can be in evaluating your watch over the phone,
+              final
+              determination
+              of trade in value is subject to our inspection of your watch.</p>
+            <a class="font-poppins text-center text-black font-semibold underline my-3" href="#">Click Here for
+              our
+              Details and
+              Declaration of Ownership (PDF format).</a>
+          </div>
+
+
+          <div class="my-5 flex justify-center items-center flex-col md:w-[45%] md:my-0 ">
+            <h3 class="font-poppins font-semibold text-xl text-center">We Accept Previously Owned Jewelry...</h3>
+            <p class="font-poppins text-center text-gray-600 font-normal my-3">...as part or all of your payment
+              towards a new item in our store.</p>
+            <p class="font-poppins text-center text-gray-600 font-normal my-3"><span class="font-semibold">How does it
+                work?</span> The first step is to give us a call or bring your jewelry to the store.
+              When you come in we will
+              analyze and weigh your gold or platinum and make you an offer based on the price of precious metal that
+              day. We will
+              issue you a credit for the stated amount, or you can select a new item from our store that day. Some
+              complex items of
+              jewelry may take longer to give you a price. Please note we do not accept gold filled or gold plated
+              items.</p>
+            <p class="font-poppins text-center text-gray-600 font-normal my-3">If you call us to discuss your jewelry
+              before your visit, please be prepared to discuss the following
+              regarding a
+              potential trade-in.</p>
+            <ul>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">What
+                is the item you are interested in purchasing?</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">What
+                are the materials of the jewelry you have? For instance Is it 24kt, 18kt or 14kt?</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">Is the
+                Jewelry stamped with a Karat mark (14kt, 18kt, 750, 585?)</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">If you
+                are interested in a new piece of jewelry, what is it?</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">Does
+                your jewelry have any diamonds, precious, or semi precious stones?</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">Is the
+                Jewelry you wish to sell made by a specific designer?</li>
+              <li class="font-poppins text-gray-600 text-base font-normal mb-3 list-disc list-inside text-left">What
+                is the approximate weight of the item?</li>
+              <p class="font-poppins text-center text-gray-600 font-normal my-3">While we make every effort to be as
+                accurate as we can be in evaluating your jewelry over the phone,
+                final determination
+                of purchase price is subject to our physical inspection of your jewelry.</p>
+              <a class="font-poppins text-center text-black font-semibold underline my-3" href="#">Click Here for our
+                Details and Declaration of Ownership (PDF format).</a>
+            </ul>
+          </div>
+        </div>
+        <p class="font-poppins text-center text-gray-600 font-normal my-3">You may wish to fill portions of this form
+          out before calling or vistiting us to help you in your
+          discussion of each
+          piece you are interested in trading in. We appreciate that you may not be able to provide all the
+          information such as
+          the specific weight of the item from your home. Please do not send us any items for evaluation without
+          calling us first!</p>
+      </section>
+      {{--| END CONTENT |--}}
+
+
+
+      {{--| FOOTER |--}}
+      <x-footer.footer />
+      {{--| END FOOTER |--}}
+    </main>
+    <x-js.my_scripts />
+  </body>
+
+</html>
+
+</html>
